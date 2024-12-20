11d5ce5b678a8383cfcf62f90614c35a0295c5d6
dobaniye <dobaniye5@gmail.com>
2024-12-20 21:09:53 +0530

    Updated web

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,18 +1,7 @@
 // src/lib/supabase/client.js
-'use client';
-
 import { createClient } from '@supabase/supabase-js';
 
-let supabase = null;
-
-if (typeof window !== 'undefined') {
-  supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-    {
-      auth: { persistSession: false }
-    }
-  );
-}
-
-export default supabase;
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+);
